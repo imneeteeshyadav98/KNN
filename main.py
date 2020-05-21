d9c73256c5d7bd3c1fdcc9d5b60aedbb012c0373
Data_Science <neeteeshyadav98@gmail.com>
2020-05-21 11:38:50 +0530

    third change

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.30,random_state=42
 from sklearn.neighbors import KNeighborsClassifier
 model=KNeighborsClassifier(n_neighbors=5)
 model.fit(X_train,y_train)
+
 y_pred=model.predict(X_test)
 
 error_rate=[]
@@ -24,4 +25,5 @@ for i in range(1,50):
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 accuracy=accuracy_score(y_test, y_pred)*100
-print(accuracy,file=open("accuracy.txt", "a"))
+print(accuracy)
+print(accuracy,file=open("accuracy.txt", "w"))
