1cd8550a17bb91930d61ad9a380bca35f34d12f1
Data_Science <neeteeshyadav98@gmail.com>
2020-05-23 13:18:49 +0530

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-dataset=pd.read_csv("Social_Network_Ads.csv")
+dataset=pd.read_csv("/home/data_science/Desktop/Mlops-ws/KNN/Social_Network_Ads.csv")
 
 X=dataset[['Age', 'EstimatedSalary']]
 
